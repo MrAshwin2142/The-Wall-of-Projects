86aa9ffbdc315f4eb5119cc27dd367d2d0cd7b32
MrAshwin2142 <ashwindhangar2142002@gmail.com>
2023-11-02 16:23:56 +0530

    Initial project setup

---
+++ projects.js
@@ -0,0 +1,55 @@
+// projects.js
+
+const projects = [
+  {
+    name: "Ashwin's Persnol Portfolio",
+    image: "./images/portfolioImg.png",
+    description: "I have built a personal portfolio website to enhance my frontend skills and showcase my work. Utilizing HTML and Bootstrap, I've crafted an interactive platform to exhibit my projects and abilities. Explore my site to discover my passion for web development and design.",
+    usedTech: "HTML,Bootstrap",
+    contributorName: "Ashwin Dhangar",
+    codeLink: "https://github.com/MrAshwin2142/portfolio/blob/master",
+    liveLink: "https://ashwin-portfolioo.netlify.app/#",
+  },
+  {
+    name: "Moviezz",
+    image: "./images/moviezz.png",
+    description: "Moviezz is a web application that replicates the Netflix experience, providing real-time movie data and a user-friendly interface for seamless movie and TV show exploration",
+    usedTech: "Reactjs",
+    contributorName: "AShwin Dhangar",
+    codeLink: "https://github.com/MrAshwin2142/moviezz",
+    liveLink: "https://tmdb-moviezz.netlify.app/",
+  },
+  {
+    name: "text to Emoji",
+    image: "https://ashwin-portfolioo.netlify.app/image/TextToEmoji.png",
+    description: "web app using HTML, CSS, and JavaScript, transforming text into emoji-encoded messages. Enhanced user experience with encryption algorithms and an intuitive UI.",
+    usedTech: "Javascript",
+    contributorName: "Ashwin Dhangar",
+    codeLink: "https://github.com/MrAshwin2142/text-to-emoji.github.io",
+    liveLink: "https://mrashwin2142.github.io/text-to-emoji.github.io/",
+  },
+  {
+    name: "Soldier Game",
+    image: "https://ashwin-portfolioo.netlify.app/image/jsGame.png",
+    description: "JavaScript game where a soldier avoids incoming fires, demonstrating real-time interaction and dynamic scoring with animation enhancements.",
+    usedTech: "Javascript",
+    contributorName: "Ashwin Dhangar",
+    codeLink: "https://github.com/MrAshwin2142/soldier.github.io",
+    liveLink: "https://mrashwin2142.github.io/soldier.github.io/",
+  },
+  {
+    name: "Super Market Billing System",
+    image: "./images/BillingSystem.jpg",
+    description: "Developed a C++ Supermarket Billing System with file handling for efficient product management and data storage, streamlining the billing process",
+    usedTech: "C++",
+    contributorName: "Ashwin Dhangar",
+    codeLink: "https://github.com/MrAshwin2142/Super-Market-Bill-System",
+    liveLink: "https://www.linkedin.com/posts/ashwin-dhangar-btech24_cpp-miniproject-project-activity-6979374060145139712-N_EY?utm_source=share&utm_medium=member_desktop",
+  },
+  
+
+
+  // Manually add more projects here
+];
+
+//   export default projects;
---
+++ script.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const projectList = document.getElementById("projectList");
+    const projectName = document.getElementById("projectName");
+    const projectImage = document.getElementById("projectImage");
+    const projectDescription = document.getElementById("projectDescription");
+    const contributor = document.getElementById("contributor");
+    const codeLink = document.getElementById("codeLink");
+    const liveLink = document.getElementById("liveLink");
+    const detailSection = document.getElementById("detailSection");
+    const usedTech = document.getElementById("usedTech");
+
+    projects.forEach((project) => {
+        const projectButton = document.createElement("button");
+        projectButton.textContent = project.name;
+        projectButton.className = "btn";
+        projectButton.title = project.usedTech;
+        // Attach an event listener to show project details when clicked
+        projectButton.addEventListener("click", () => {
+            displayProjectDetails(project);
+        });
+
+        // Append the project button to the project list
+        projectList.appendChild(projectButton);
+    });
+    function displayProjectDetails(project) {
+        if (!detailSection.classList.contains('hidden') && projectName.innerText === project.name) {
+            detailSection.classList.add('hidden');
+        } else {
+            console.log("hello")
+            detailSection.classList.remove('hidden');
+        }
+        projectName.innerText = project.name;
+        projectDescription.innerText = project.description;
+        contributor.innerText = project.contributorName;
+        usedTech.innerText = project.usedTech;
+        codeLink.href = project.codeLink;
+        liveLink.href = project.liveLink;
+        if (project.image) {
+            projectImage.src = project.image;
+        } else {
+            projectImage.src = './images/default-image.jpg';
+        }
+    }
+});
