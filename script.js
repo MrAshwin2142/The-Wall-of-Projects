fead521806bd737b31bcffcec47afb7f3025ddea
abe <abel.rogers@gmail.com>
2023-11-08 19:01:51 +0000

    Update script.js
    I took the liberty of optimising your script code a little, hope you don't mind?? Can do more...
    ValidURL check
    Better error message handling, esp. for language change
    more!

--- script.js
+++ script.js
@@ -1,66 +1,74 @@
+// Define the functions outside of the DOMContentLoaded event
+function displayProjectDetails(project, elements) {
+  const {
+    projectName,
+    projectDescription,
+    contributor,
+    usedTech,
+    codeLink,
+    liveLink,
+    liveLinkError,
+    projectImage,
+    detailSection,
+  } = elements;
+
+  const shouldBeHidden =
+    !detailSection.classList.contains("hidden") &&
+    projectName.textContent === project.name;
+  detailSection.classList.toggle("hidden", shouldBeHidden);
+
+  projectName.textContent = project.name;
+  projectDescription.textContent = project.description;
+  contributor.textContent = project.contributorName;
+  usedTech.textContent = project.usedTech;
+  codeLink.href = project.codeLink;
+
+  liveLink.href = project.liveLink || "javascript:void(0);";
+  liveLinkError.textContent = project.liveLink
+    ? ""
+    : "Live link is not available";
+
+  projectImage.src = project.image || "./images/default-image.jpg";
+}
+
+// Wait until the DOM is fully loaded before running the script
 document.addEventListener("DOMContentLoaded", () => {
-    const projectList = document.getElementById("projectList");
-    const projectName = document.getElementById("projectName");
-    const projectImage = document.getElementById("projectImage");
-    const projectDescription = document.getElementById("projectDescription");
-    const contributor = document.getElementById("contributor");
-    const codeLink = document.getElementById("codeLink");
-    const liveLink = document.getElementById("liveLink");
-    const detailSection = document.getElementById("detailSection");
-    const usedTech = document.getElementById("usedTech");
-    const contributorTable = document.getElementById("contributorTable");
-    const liveLinkError = document.getElementById("liveLinkError");
+  const elements = {
+    projectName: document.getElementById("projectName"),
+    projectImage: document.getElementById("projectImage"),
+    projectDescription: document.getElementById("projectDescription"),
+    contributor: document.getElementById("contributor"),
+    codeLink: document.getElementById("codeLink"),
+    liveLink: document.getElementById("liveLink"),
+    detailSection: document.getElementById("detailSection"),
+    usedTech: document.getElementById("usedTech"),
+    contributorTable: document.getElementById("contributorTable"),
+    liveLinkError: document.getElementById("liveLinkError"),
+  };
 
-    const starContributor = {};
+  const starContributor = {};
+  const projectList = document.getElementById("projectList");
 
+  projects.forEach((project) => {
+    const projectButton = document.createElement("button");
+    projectButton.textContent = project.name;
+    starContributor[project.contributorName] =
+      (starContributor[project.contributorName] || 0) + 1;
+    projectButton.className = "btn";
+    projectButton.title = project.usedTech;
 
-    projects.forEach((project) => {
-        const projectButton = document.createElement("button");
-        projectButton.textContent = project.name;
-        if (starContributor[project.contributorName]) {
-            starContributor[project.contributorName] += 1;
-        } else {
-            // starContributor.set(project.contributorName,  Number(1));
-            starContributor[project.contributorName]=  Number(1);
-        }
-        projectButton.className = "btn";
-        projectButton.title = project.usedTech;
-        // Attach an event listener to show project details when clicked
-        projectButton.addEventListener("click", () => {
-            displayProjectDetails(project);
-        });
-
-        // Append the project button to the project list
-        projectList.appendChild(projectButton);
+    projectButton.addEventListener("click", () => {
+      displayProjectDetails(project, elements);
     });
-    // console.log(starContributor);
-    for(const key in starContributor){
-        const nameNproject= document.createElement("tr");
-        nameNproject.innerHTML=`<td>${key}</td><td>${starContributor[key]}</td>`
-        contributorTable.append(nameNproject); 
+
+    projectList.appendChild(projectButton);
+  });
+
+  Object.entries(starContributor).forEach(
+    ([contributorName, projectsCount]) => {
+      const row = document.createElement("tr");
+      row.innerHTML = `<td>${contributorName}</td><td>${projectsCount}</td>`;
+      elements.contributorTable.appendChild(row);
     }
-    function displayProjectDetails(project) {
-        if (!detailSection.classList.contains('hidden') && projectName.innerText === project.name) {
-            detailSection.classList.add('hidden');
-        } else {
-            detailSection.classList.remove('hidden');
-        }
-        projectName.innerText = project.name;
-        projectDescription.innerText = project.description;
-        contributor.innerText = project.contributorName;
-        usedTech.innerText = project.usedTech;
-        codeLink.href = project.codeLink;
-        if (project.liveLink) {
-            liveLink.href = project.liveLink;
-            liveLinkError.textContent = ""; // Clear the error message if live link is available
-        } else {
-            liveLink.href = "#"; // Set a placeholder link
-            liveLinkError.textContent = "Live link is not available"; // Display the error message
-        }
-        if (project.image) {
-            projectImage.src = project.image;
-        } else {
-            projectImage.src = './images/default-image.jpg';
-        }
-    }    
+  );
 });
